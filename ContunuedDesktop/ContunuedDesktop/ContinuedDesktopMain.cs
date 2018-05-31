e021e4286023b6d33618c28e7973f1a9bf1e7fdc
lordsokol <39807379+lordsokol@users.noreply.github.com>
2018-05-31 13:54:00 -0700

    Initial upload of solution
    initial solution upload

---
+++ ContunuedDesktop/ContunuedDesktop/ContinuedDesktopMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContunuedDesktop
+{
+    public partial class ContinuedDesktopMain : Form
+    {
+        public ContinuedDesktopMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ ContunuedDesktop/ContunuedDesktop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ContunuedDesktop
+{
+    static class Program
+    {
+        /// <summary>
+        /// Keep the Desktop Area "alive" to prevent screensaver from interrupting file parsing.
+        /// This program spoofs the excape key being pressed to circumvent the 
+        /// inaccessable (due to access control policies) screen saver from kicking in.  
+        /// This behavior was problematic when trying to view large data files being parsed 
+        /// prgramatically and having the screen time out ...  
+        /// 
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ContinuedDesktopMain Desktop = new ContinuedDesktopMain();
+
+            controlTimer();
+            Application.Run(Desktop);
+        }
+
+
+        public static void controlTimer()
+        {
+            Timer timer = new Timer();
+            //timer Interval in minutes
+            timer.Interval = (int)(TimeSpan.TicksPerMinute * 14 / TimeSpan.TicksPerMillisecond);
+            timer.Tick += (sender, args) => { Cursor.Position = new System.Drawing.Point(Cursor.Position.X + 10, Cursor.Position.Y + 10); };
+            timer.Tick += (sender, args) => { controlWindows.controlW("^{ESC}"); };
+            timer.Start();
+        }
+    }
+}
+
